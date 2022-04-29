12384cb8a0366ba76ac5ceeefa5f311d39e5f227
kesavanmurali <k7mraj@gmail.com>
2022-04-29 21:56:30 +0530

    Initial setup

---
+++ kesavan-petclinic/src/main/java/com/kesavan/petclinic/KesavanPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.kesavan.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KesavanPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KesavanPetclinicApplication.class, args);
+	}
+
+}
---
+++ kesavan-petclinic/src/test/java/com/kesavan/petclinic/KesavanPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesavan.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KesavanPetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
