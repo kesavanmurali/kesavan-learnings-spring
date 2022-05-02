958e6de829a04b0f62e12938dd4f515b32a163a2
kesavanmurali <k7mraj@gmail.com>
2022-05-02 16:33:17 +0530

    Added Owners page and Controller. Closes #9.

---
+++ kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.kesavan.petclinic.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/index.html", "/index"})
+    public String index(){
+        return "owners/index";
+    }
+
+}
