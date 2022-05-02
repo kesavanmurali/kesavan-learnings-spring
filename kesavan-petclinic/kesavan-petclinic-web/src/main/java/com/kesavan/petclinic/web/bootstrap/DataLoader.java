60af7309564bcdb420315cae2182e22e5e4e4919
kesavanmurali <k7mraj@gmail.com>
2022-05-02 19:02:02 +0530

    Displaying list of owners and Vets. Closes #11.

--- kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/bootstrap/DataLoader.java
+++ kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.kesavan.petclinic.data.model.Owner;
 import com.kesavan.petclinic.data.model.Vet;
 import com.kesavan.petclinic.data.services.OwnerService;
 import com.kesavan.petclinic.data.services.VetService;
-import com.kesavan.petclinic.data.services.map.OwnerServiceMap;
-import com.kesavan.petclinic.data.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        this.ownerService = new OwnerServiceMap();
-        this.vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
--- kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/controllers/VetController.java
+++ kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.kesavan.petclinic.web.controllers;
 
+import com.kesavan.petclinic.data.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("vets", this.vetService.findAll());
         return "vets/index";
     }
 
