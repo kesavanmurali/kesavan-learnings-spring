f4dc73a99da6b95d2627ed65c2fac6d5b4719a0d
kesavanmurali <k7mraj@gmail.com>
2022-05-02 14:11:02 +0530

    Multi-module project setup with Issue fix. Closes #2.

---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/KesavanPetclinicDataApplication.java
@@ -0,0 +1,13 @@
+package com.kesavan.petclinic.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KesavanPetclinicDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KesavanPetclinicDataApplication.class, args);
+	}
+
+}
---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/test/java/com/kesavan/petclinic/data/KesavanPetclinicDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kesavan.petclinic.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = KesavanPetclinicDataApplication.class)
+class KesavanPetclinicDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
