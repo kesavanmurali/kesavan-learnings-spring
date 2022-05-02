f06713bf040e33dd09ce237176194fe21d86df07
kesavanmurali <k7mraj@gmail.com>
2022-05-02 14:28:36 +0530

    Added Ids to models. Closes #4.

--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/BaseEntity.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/BaseEntity.java
@@ -3,9 +3,12 @@ package com.kesavan.petclinic.data.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class PetType extends BaseEntity{
+public class BaseEntity implements Serializable {
 
-    private String name;
+    private Long id;
+
 }
