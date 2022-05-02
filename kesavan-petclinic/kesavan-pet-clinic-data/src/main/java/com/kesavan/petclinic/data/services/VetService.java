58b86f68bd115342192c369322cde5f560f21056
kesavanmurali <k7mraj@gmail.com>
2022-05-02 14:57:10 +0530

    Introduced a common interface. Closes #5.

--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/VetService.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/VetService.java
@@ -4,14 +4,8 @@ import com.kesavan.petclinic.data.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService  extends CrudService<Vet, Long> {
 
     Vet findByLastName(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 
 }
