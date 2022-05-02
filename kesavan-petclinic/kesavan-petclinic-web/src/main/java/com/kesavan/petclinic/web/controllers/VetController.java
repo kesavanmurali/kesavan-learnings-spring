3f45fa260b34ee4e2d08917e8890fe0343f491cf
kesavanmurali <k7mraj@gmail.com>
2022-05-02 16:29:32 +0530

    Added Vets page and Controller. Closes #8.

---
+++ kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/controllers/VetController.java
@@ -0,0 +1,14 @@
+package com.kesavan.petclinic.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index.html", "/vets/index"})
+    public String index(){
+        return "vets/index";
+    }
+
+}
