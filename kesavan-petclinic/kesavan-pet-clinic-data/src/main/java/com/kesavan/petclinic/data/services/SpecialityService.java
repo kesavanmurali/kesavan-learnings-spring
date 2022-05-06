7f60d3582cd5088642e1fa1785b0031e7f5ed227
kesavanmurali <k7mraj@gmail.com>
2022-05-06 15:01:33 +0530

    Introduced Speciality Map Service. Closes #26.

---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.kesavan.petclinic.data.services;
+
+import com.kesavan.petclinic.data.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/map/SpecialityMapService.java
@@ -0,0 +1,37 @@
+package com.kesavan.petclinic.data.services.map;
+
+import com.kesavan.petclinic.data.model.Speciality;
+import com.kesavan.petclinic.data.services.SpecialityService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
