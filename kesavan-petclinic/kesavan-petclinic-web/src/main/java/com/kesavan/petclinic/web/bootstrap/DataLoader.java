8ea4a4d1217d4f8a549ffa1dfad91ee835e6b9d9
kesavanmurali <k7mraj@gmail.com>
2022-05-06 14:19:31 +0530

    Added Bootstrap data for PetTypeService. Closes #24.

--- kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/bootstrap/DataLoader.java
+++ kesavan-petclinic/kesavan-petclinic-web/src/main/java/com/kesavan/petclinic/web/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.kesavan.petclinic.web.bootstrap;
 
 import com.kesavan.petclinic.data.model.Owner;
+import com.kesavan.petclinic.data.model.PetType;
 import com.kesavan.petclinic.data.model.Vet;
 import com.kesavan.petclinic.data.services.OwnerService;
+import com.kesavan.petclinic.data.services.PetTypeService;
 import com.kesavan.petclinic.data.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,31 +14,38 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        PetType dog = new PetType();
+        dog.setName("Candy");
+        PetType savedDogType = this.petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Catty");
+        PetType savedCatType = this.petTypeService.save(cat);
+
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Kesavan");
         owner1.setLastName("Murali");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rajendran");
         owner2.setLastName("Thiruvengatam");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Prabhavathi");
         vet1.setLastName("Murali");
 
