85d37715492a5a6dd674dc9d3ce0c2380d708769
kesavanmurali <k7mraj@gmail.com>
2022-05-07 12:44:55 +0530

    Created Owner to a JPA entity. Closes #30.

--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/PetType.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/PetType.java
@@ -3,9 +3,15 @@ package com.kesavan.petclinic.data.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 @Getter
 @Setter
+@Entity
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
+
 }
