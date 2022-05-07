789ca1c60da6d24c5a39ce1e9482b793ebf46ca3
kesavanmurali <k7mraj@gmail.com>
2022-05-07 12:51:34 +0530

    Converted vet to a JPA entity. Closes #31.

--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/PetType.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/PetType.java
@@ -5,10 +5,12 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
     @Column(name = "name")
--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/Speciality.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/Speciality.java
@@ -2,11 +2,19 @@ package com.kesavan.petclinic.data.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
 }
