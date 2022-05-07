9d54d8d5ff1b8e8f42b870631172783e1427a4af
kesavanmurali <k7mraj@gmail.com>
2022-05-07 14:37:33 +0530

    Introduced map-based service for Visits. Closes #37.

---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.kesavan.petclinic.data.services;
+
+import com.kesavan.petclinic.data.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
---
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.kesavan.petclinic.data.services.map;
+
+import com.kesavan.petclinic.data.model.Visit;
+import com.kesavan.petclinic.data.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit == null || visit.getPet() == null || visit.getPet().getOwner() == null  || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
