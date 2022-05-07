ecf0cf27b12f7e516610956e7e28ee5a904c3a21
kesavanmurali <k7mraj@gmail.com>
2022-05-07 12:58:34 +0530

    Converted Visits to a JPA entity. Closes #32.

--- kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/Visit.java
+++ kesavan-petclinic/kesavan-pet-clinic-data/src/main/java/com/kesavan/petclinic/data/model/Visit.java
@@ -3,14 +3,23 @@ package com.kesavan.petclinic.data.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
-    private String description;
+    @Column(name = "description")
+    private String desription;
+
+    @Column(name = "date")
     private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
 }
